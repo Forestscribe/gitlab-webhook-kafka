aa0b3f0f81ee51cfa5aefab631ffdab2dea3cf92
Pierre Tardy <tardyp@gmail.com>
2017-05-16 17:33:28 +0200

    simplify unit tests

--- test_e2e.py
+++ test_e2e.py
@@ -33,20 +33,18 @@ def application():
 def test_json(application):
     consumer = KafkaConsumer('testgitlabjson', group_id='test', fetch_max_wait_ms=100,
                              bootstrap_servers=[os.environ['KAFKA_SERVER']])
-    partitions = consumer.poll(1000)
     requests.post("http://localhost:5000", json={'a': 1, 'b': 2})
-    partitions = consumer.poll(10000)
-    message = partitions.values()[0][0]
+    message = consumer.next()
     assert message.topic == 'testgitlabjson'
     assert json.loads(message.value) == dict(a=1, b=2)
+    consumer.close()
 
 
 def test_bson(application):
     consumer = KafkaConsumer('testgitlabbson', fetch_max_wait_ms=100, group_id='test',
                              bootstrap_servers=[os.environ['KAFKA_SERVER']])
-    partitions = consumer.poll(1000)
     requests.post("http://localhost:5000", json={'a': 1, 'b': 2})
-    partitions = consumer.poll(10000)
-    message = partitions.values()[0][0]
+    message = consumer.next()
     assert message.topic == 'testgitlabbson'
     assert bson.loads(message.value) == dict(a=1, b=2)
+    consumer.close()
