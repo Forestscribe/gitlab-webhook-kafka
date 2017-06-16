7b28a5d6d098ceff5984d128be39ed94e7fd15fe
Pierre Tardy <tardyp@gmail.com>
2017-06-16 17:36:16 +0200

    event forwarder

---
+++ forward_event.py
@@ -0,0 +1,31 @@
+import hashlib
+import logging
+import sys
+
+import bson
+import click
+import requests
+from kafka import KafkaConsumer
+
+
+@click.command()
+@click.option('--kafka-server', envvar='KAFKA_SERVER', required=True)
+@click.option('--forward', required=True, envvar='FORWARD_SERVER', help='forward event to http hook')
+@click.option('--debug/--no-debug', '-d', help='debug logging')
+def forward(kafka_server, debug=False, forward=None):
+    """Simple program that forward events from gitlab queue."""
+    if debug:
+        logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+
+    server_hash = hashlib.sha1(forward).hexdigest()[:5]
+
+    consumer = KafkaConsumer('gitlabbson', group_id="forwardevent" + server_hash, fetch_max_wait_ms=10000,
+                             bootstrap_servers=[kafka_server])
+    for x in consumer:
+        event = bson.loads(x.value)
+        r = requests.post(forward, json=event)
+        print(r.content)
+
+
+if __name__ == '__main__':
+    forward()
